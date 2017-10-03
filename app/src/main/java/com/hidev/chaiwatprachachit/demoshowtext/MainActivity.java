a6d7b8874b6804e495a04efbedc37eaa18725aaa
chaizakao <chai_2117@hotmail.com>
2017-10-03 10:57:28 +0700

    Initial commit

---
+++ app/src/main/java/com/hidev/chaiwatprachachit/demoshowtext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hidev.chaiwatprachachit.demoshowtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
